abfb29872db784c10fe6d62b68c331ffcd241fd8
kacperpucylo <pucylek@wp.pl>
2024-01-02 20:50:00 +0100

    enemy ai, victory and game over

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ impl State {
 		let mut rng = RandomNumberGenerator::new();
 		let map_builder = MapBuilder::new(&mut rng);
 		spawn_player(&mut ecs, map_builder.player_start);
+		spawn_amulet(&mut ecs, map_builder.amulet_start);
 		map_builder
 			.rooms
 			.iter()
@@ -54,6 +55,46 @@ impl State {
 			input_systems: build_input_scheduler(),
 			player_systems: build_player_scheduler(),
 			monster_systems: build_monster_scheduler(),
+		}
+	}
+
+	fn reset_game_state(&mut self) {
+		self.ecs = World::default();
+		self.resources = Resources::default();
+		let mut rng = RandomNumberGenerator::new();
+		let map_builder = MapBuilder::new(&mut rng);
+		spawn_player(&mut self.ecs, map_builder.player_start);
+		spawn_amulet(&mut self.ecs, map_builder.amulet_start);
+		map_builder.rooms
+			.iter()
+			.skip(1)
+			.map(|r| r.center())
+			.for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+		self.resources.insert(map_builder.map);
+		self.resources.insert(Camera::new(map_builder.player_start));
+		self.resources.insert(TurnState::AwaitingInput);
+	}
+
+	fn game_over(&mut self, ctx: &mut BTerm) {
+		ctx.set_active_console(2);
+		ctx.print_color_centered(2, RED, BLACK, "Your quest is over");
+		ctx.print_color_centered(4, WHITE, BLACK, "Slain by a monster, your journey has come to an end");
+		ctx.print_color_centered(8, YELLOW, BLACK, "You can always try again");
+		ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to retry");
+
+		if let Some(VirtualKeyCode::Key1) = ctx.key {
+			self.reset_game_state();
+		}
+	}
+
+	fn victory(&mut self, ctx: &mut BTerm) {
+		ctx.set_active_console(2);
+		ctx.print_color_centered(2, GREEN, BLACK, "You won!");
+		ctx.print_color_centered(4, WHITE, BLACK, "You put on the amulet and feel its power");
+		ctx.print_color_centered(5, WHITE, BLACK, "Your town is saved and you can return to your normal life");
+		ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again");
+		if let Some(VirtualKeyCode::Key1) = ctx.key {
+			self.reset_game_state();
 		}
 	}
 }
@@ -82,6 +123,12 @@ impl GameState for State {
 			TurnState::MonsterTurn => self
 				.monster_systems
 				.execute(&mut self.ecs, &mut self.resources),
+			TurnState::GameOver => {
+				self.game_over(ctx);
+			},
+			TurnState::Victory => {
+				self.victory(ctx);
+			}
 		}
 		render_draw_buffer(ctx).expect("Render error");
 	}
